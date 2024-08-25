7557aa5de991c1740192f9f46009a5a497d73394
SunKnight002 <leosun6582@gmail.com>
2024-08-25 18:27:42 +0800

    安裝FontAwsome 相關安裝包，基礎設置router，還未建立heade所以沒辦法實現連線到其他頁面，header將會在空閒的時候建立

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
-import './assets/main.css'
+// import './assets/main.css'
+import "@/assets/css/reset.css"
 
 import { createApp } from 'vue'
+// 以下是路由器 vue-router
+import { createWebHistory } from 'vue-router'
+// 以下是必要的步驟
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+// 這是狀態器 pinia
 import { createPinia } from 'pinia'
 
+import createRouter from './router/routes'
 import App from './App.vue'
-import router from './router'
 
+// 添加Font Awesome圖標到庫中
+library.add(faBars);
+
+// 以下是路由器 vue-router 相關設置
+const router = createRouter(createWebHistory())
+// 以下是預設設置
 const app = createApp(App)
-
+// 下面這行必須要加，由於要使用fontawesome
+app.component('font-awesome-icon', FontAwesomeIcon);
+// 以下是路由器  vue-router 相關設置
+app.use(router).mount('#app')
 app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
---
+++ src/router/routes.js
@@ -0,0 +1,27 @@
+import {createRouter} from 'vue-router'
+import Fontpage from '../views/fontpage/fontpage.vue';
+import Homepage from '../views/homepage/home.vue';
+// 左邊是你宣告的名稱，可以跟你的 .vue檔案名稱相同。
+// 右邊是你的 .vue 檔案路徑。
+
+const routes = [
+  {
+    path: '/',
+    component: Fontpage
+  },
+
+  {
+    path: '/home',
+    component: Homepage
+  },
+//   這邊放置需要增加的頁面以及設置她可能的網址
+// path:'/後面的內容是網址'
+//  component: 這邊要帶入你在最上方宣告的元件
+]
+
+export default function (history) {
+  return createRouter({
+    history,
+    routes
+  })
+}
