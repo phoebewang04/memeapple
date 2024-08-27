724e9efe5ae0eacd6fe72cdb523ce6faefa0853a
yunyylin <hihidp2a07@gmail.com>
2024-08-27 09:43:18 +0800

    minigame新增gsap動畫、img照片

--- minigame.js
+++ minigame.js
@@ -14,3 +14,44 @@ window.addEventListener('scroll', () => {
     gateRight.style.left = value * -0.2 + 'px';
     
 });
+
+//------------------------------------------
+
+const ghostText = new SplitType(".split_text");
+gsap.registerPlugin(ScrollTrigger);
+
+//建立gsap時間軸
+const tlGhost = gsap.timeline();
+
+//設置滾動觸發動畫
+tlGhost.from(".cemetery_text .char", {
+  filter: "blur(20px)",
+  willchange: "filter",
+  stagger: 0.07, //間隔
+  scrollTrigger: {
+    trigger: ".cemetery_text",
+    start: "top center",
+    end: "bottom top+=300", //元素的底部超過視口頂部 100px 時
+    scrub: 1.5,
+    // markers: true,
+    ease: "power4.out",
+    // pin: true
+  }
+});
+
+/*
+trigger: 定義觸發動畫的元素
+start: 動畫開始的位置
+end: 動畫結束的位置
+scrub: 為 true 時，動畫會根據滾動的速度來控制播放進度，使動畫與滾動同步
+ease: 動畫的緩動效果為 power4.out，這會讓動畫逐漸變慢並優雅地停止
+ */
+
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
