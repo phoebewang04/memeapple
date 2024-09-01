4d8aacce8daca48eb898c864c2ca8aada936510c
SunKnight002 <leosun6582@gmail.com>
2024-09-02 02:17:24 +0800

    切版籠中鳥

--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -30,7 +30,7 @@ export default {
             <li><router-link to="/index/">首頁</router-link></li>
             <li><router-link to="/Membermanage/">會員專區</router-link></li>
             <li><router-link to="/Minigame/">線上體驗</router-link></li>
-            <li><router-link to="/Branch/">分館介紹</router-link></li>
+            <!-- <li><router-link to="/Branch/">分館介紹</router-link></li>
             <li><router-link to="/Announcement/">最新消息</router-link></li>
             <li><router-link to="/Popupcard/">彈跳甲魚</router-link></li>
             <li><router-link to="/Theme_Time/">快樂地城</router-link></li> 
@@ -39,7 +39,7 @@ export default {
             <li><router-link to="/preorder/">預定資料</router-link></li>
             <li><router-link to="/orderinform/">資料確認</router-link></li>
             <li><router-link to="/Theme/">主題介紹</router-link></li>
-            <li><router-link to="/pay/">結帳</router-link></li> 
+            <li><router-link to="/pay/">結帳</router-link></li>  -->
         </ul>
     </header>
 </template>
