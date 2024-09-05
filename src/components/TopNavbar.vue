261a18f8548f7a8545eab687ef95e32f31f4074c
SunKnight002 <leosun6582@gmail.com>
2024-09-05 20:53:20 +0800

    feat: 新增header 的漢堡功能 以及彈跳卡片結合SweetAlert2

--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -1,39 +1,39 @@
 <script>
-import { ref } from 'vue';
 import '../assets/css/style.css'
 
 export default {
-    setup() {
-    const menuBarVisible = ref(false);
-
-    const showMenuBar = () => {
-      console.log('Menu bar icon clicked');
-      menuBarVisible.value = !menuBarVisible.value;
+  data(){
+    return{
+      visible_active:false
     };
-
-    return {
-      menuBarVisible,
-      showMenuBar
-    };
+  },
+  methods: {
+    showMenuBar(){
+      if(window.innerWidth <= 650){
+        this.visible_active = !this.visible_active;
+      }
+    }
+  },
+  created(){
+      if(window.innerWidth > 650){
+        this.visible_active = true;
+      }
   }
 };
 </script>
 
 <template>
     <header class="top">
-        <!-- 這是logo，點了之後回到index.html -->
         <router-link to="/"><img src="../assets/img/memelogo.svg" alt=""></router-link>
-        <!-- <i class="fa fa-bars" aria-hidden="true" @click="showMenuBar()"></i> -->
         <font-awesome-icon class="fa" icon="bars" @click="showMenuBar"></font-awesome-icon>
-        <!-- 這是列表，透過router連結到其他 .vue 頁面 -->
-        <ul id="menuBar" :class="{ 'visible': menuBarVisible }">
+        <ul id="menuBar" :class="{ visible: visible_active }">
             <li><router-link to="/index/">首頁</router-link></li>
             <li><router-link to="/Membermanage/">會員專區</router-link></li>
             <li><router-link to="/Minigame/">線上體驗</router-link></li>
-            <!-- <li><router-link to="/Branch/">分館介紹</router-link></li>
+            <li><router-link to="/Branch/">分館介紹</router-link></li>
             <li><router-link to="/Announcement/">最新消息</router-link></li>
             <li><router-link to="/Popupcard/">彈跳甲魚</router-link></li>
-            <li><router-link to="/Theme_Time/">快樂地城</router-link></li>
+            <!-- <li><router-link to="/Theme_Time/">快樂地城</router-link></li>
             <li><router-link to="/Theme_Lock/">武石監</router-link></li>
             <li><router-link to="/ThemeAlien/">逃出虛空</router-link></li>
             <li><router-link to="/preorder/">預定資料</router-link></li>
